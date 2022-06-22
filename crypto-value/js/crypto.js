3dbba8e1d4faf4c2e06cea354a8869bb9f196794
Renato Bittar <renatobg@gmail.com>
2022-06-22 12:48:30 +0100

    adicionada api para btc

--- crypto-value/js/crypto.js
+++ crypto-value/js/crypto.js
@@ -4,5 +4,15 @@ var dia = String(data.getDate()).padStart(2, '0');
 var mes = String(data.getMonth() + 1).padStart(2, '0');
 var ano = data.getFullYear();
 dataAtual = dia + '/' + mes + '/' + ano;
-
+// Troca o texto pela data atual coletada acima
 document.getElementById("data").textContent = (dataAtual);
+
+let ws = new WebSocket('wss://stream.binance.com:9443/ws/btcusdt@trade');
+let stockPriceElement = document.getElementById('btcvalue');
+
+console.log(ws);
+ws.onmessage = (event) => {
+    let stockObject = JSON.parse(event.data);
+    stockPriceElement.innerText = parseFloat(stockObject.p).toFixed(2);
+}
+
