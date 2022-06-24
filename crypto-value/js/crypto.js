5a4c73e5196e48e8e531bb5dce7e465837550e74
Renato Bittar <renatobg@gmail.com>
2022-06-24 22:44:50 +0100

    página crypto funcionando

--- crypto-value/js/crypto.js
+++ crypto-value/js/crypto.js
@@ -7,16 +7,30 @@ dataAtual = dia + '/' + mes + '/' + ano;
 // Troca o texto pela data atual coletada acima
 document.getElementById("data").textContent = (dataAtual);
 
+
 let ws = new WebSocket('wss://stream.binance.com:9443/ws/btcusdt@trade');
 let stockPriceElement = document.getElementById('btcvalue');
+
 
 console.log(ws);
 ws.onmessage = (event) => {
     let stockObject = JSON.parse(event.data);
-    stockPriceElement.innerText = parseFloat(stockObject.p).toFixed(2);
+    //stockPriceElement.innerText = parseFloat(stockObject.p).toFixed(2);
+    preco = parseFloat(stockObject.p).toFixed(2);
+    stockPriceElement.innerText = preco;
+    globalThis(preco)
+    
+} 
+
+
+
+
+
+
+function calcular(){
+    var form = document.getElementById("myForm");
+    //var data = new FormData();
+    //data.append("dinheiro", document.getElementById("valorform").value);
+    var valornovo = document.getElementById('valorform').value;
+    dolarBtc.innerHTML = `${valornovo} usd = ${(valornovo / preco).toFixed(8)} BTC`
 }
-
-var data = new FormData();
-data.append("dinheiro", document.getElementById("valor").value);
-
-console.log(data);
